6ad502d3a62673af5fdc62d62ba68626e1c80110
m1st4k3nn <kostadin2007y@gmail.com>
2025-10-03 23:36:05 +0300

    Update changelog with detailed notes and contact info

--- pages/api/getPluginData.js
+++ pages/api/getPluginData.js
@@ -1,6 +1,19 @@
 export default function handler(req, res) {
   res.status(200).json({
-    latestVersion: "1.4.8",
-    changelog: []
+    latestVersion: "1.0.0",
+    Announcement: "Bugs, issues, feature requests? Please send us a message by going to the settings menu and clicking on 'Contact us'",
+    changelog: {
+      "1.0.0": {
+        UpdateName: "Initial (kinda stable) Release",
+        Changes: [
+          "Core intended functionality",
+          "Basic user customization features",
+          "AI responses do not stream anymore, they show when they are ready, roblox limitations really, but that should be better for user experience and it doesnt affect response quality",
+        ],
+        Notes: [
+          "If you had used the plugin before, chat data has been deleted (new datastore structure)",
+        ]
+      }
+    }
   });
 }
